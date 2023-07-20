786f62698fcb434f858b5d5444aa3a244f2ccab3
Gabriel_Lima <gabriel58221@gmail.com>
2023-07-20 20:22:56 -0300

    Introduce the home page

--- src/Pages/Details/Index.jsx
+++ src/Pages/Details/Index.jsx
@@ -1,18 +1,46 @@
-import { Container } from "./style";
+import { Container, Links, Content } from "./style";
 import { Button } from "../../Components/Button";
+import { ButtonText } from "../../Components/ButtonText";
 import { Header } from "../../Components/Header";
 import { Section } from "../../Components/Section";
+import { Tag } from "../../Components/Tags";
 
 export function Details() {
   return (
     <Container>
       <Header/>
 
-      <Section title="Links úteis">
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota"/>
 
-      </Section>
+          <h1>Introdução ao React</h1>
 
-      <Button title="Voltar" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Beatae ea nulla laboriosam odio provident commodi?
+             A temporibus perferendis ullam sapiente, magni suscipit odit,
+             A temporibus perferendis ullam sapiente, magni suscipit odit,
+             consectetur dicta, consequatur nam iusto praesentium sit.
+             consectetur dicta, consequatur nam iusto praesentium sit.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Express"/>
+            <Tag title="Node"/>
+          </Section>
+
+          <Button title="Voltar" />
+
+        </Content>
+      </main>
     </Container>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './Styles/global'; // aplicação css global
 
 import theme from './Styles/theme'; // importando o css global
 
-import {Details} from './Pages/Details/Index' // importando a pagina details
+import {Home} from './Pages/Home/index' // importando a pagina details
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
